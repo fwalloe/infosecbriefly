a55ec1aec57f4d914a76167bf982c653997069d1
fwalloe <code@fredrikwalloe.com>
2016-03-21 22:07:03 +0100

    google ads as .js

---
+++ hubpress/scripts/googleAds.js
@@ -0,0 +1,10 @@
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<!-- InfoSec-responsive -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-6061586253674944"
+     data-ad-slot="2780930118"
+     data-ad-format="auto"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
